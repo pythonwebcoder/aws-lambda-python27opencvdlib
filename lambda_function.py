c60c92919cb6a220b46b8aad641b630889b15da6
Jason <jdudash@redhat.com>
2017-09-20 23:29:21 -0400

    s3 tweaking

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,26 @@ import numpy as np
 import os
 import boto3
 
-print os.environ
+#print os.environ
 
 # Setup AWS access and S3 buckets
 S3OUTBUCKET = os.environ.get('S3OUTBUCKET')
 S3INBUCKET = os.environ.get('S3INBUCKET')
+TESTS3KEY = '59bf0c6bbb886c5a69d758e3-59bf0c6c301edb5a46a1643c.png' # replace with your object key
 s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+try:
+    s3.Bucket(S3INBUCKET).download_file(TESTS3KEY, '59bf0c6bbb886c5a69d758e3-59bf0c6c301edb5a46a1643c.png')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
 
 def lambda_handler(event, context):
 	print "OpenCV version=", cv2.__version__
 	print "np version=", np.__version__
-	return "It works!"
+	print "context=", context
+	return "yay, it works!"
 
 if __name__ == "__main__":
 	lambda_handler(42, 42)
