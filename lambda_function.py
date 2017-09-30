bd0b5239322c165f144aa90ca0e63e47a2e8070b
Jason <jdudash@redhat.com>
2017-09-29 22:37:12 -0400

    pulling unnecessary s3 commands

--- lambda_function.py
+++ lambda_function.py
@@ -4,21 +4,13 @@ import numpy as np
 import os
 import boto3
 import botocore
+import urllib
 
 #print os.environ
 
 # Setup AWS access and S3 buckets
 S3OUTBUCKET = os.environ.get('S3OUTBUCKET')
 S3INBUCKET = os.environ.get('S3INBUCKET')
-TESTS3KEY = '59bf0c6bbb886c5a69d758e3-59bf0c6c301edb5a46a1643c.png' # replace with your object key
-s3 = boto3.resource('s3')
-try:
-    s3.Bucket(S3INBUCKET).download_file(TESTS3KEY, '59bf0c6bbb886c5a69d758e3-59bf0c6c301edb5a46a1643c.png')
-except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
-    else:
-        raise
 
 def lambda_handler(event, context):
 	print "OpenCV version=", cv2.__version__
@@ -26,8 +18,9 @@ def lambda_handler(event, context):
 	print "context=", context
 	print "event=", event
 	
-	#image_key = event.Records[0].s3.object.key;
-	#print image_key
+	bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'])
+	print key
 
 	return "yay, it works!"
 
